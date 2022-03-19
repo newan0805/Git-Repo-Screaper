c593ad678472bbe36b887b54df1ace8cf1242e15
newan0805 <newan0805@gmail.com>
2022-03-20 05:20:54 +0530

    2

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 const urlBase = "https://github.com/newan0805?tab=repositories";
 
-app.get('/', (req, res)=> {
-    axios(urlBase)
-      .then((getHtml) => {
-        const html = getHtml.data;
-        const $ = cheerio.load(html);
-        const dataFromGit = [];
-        $(".col-10", html).each(function () {
-          const image =
-            "https://img.icons8.com/clouds/500/000000/motivation-daily-quotes.png";
-          const title = $(this).find("a").text().replace("\n        ", "");
-          const desciption = $(this)
-            .find("p")
-            .text()
-            .replace("\n          ", "")
-            .replace("\n        ", "");
-          const link = $(this).find("a").attr("href");
-          dataFromGit.push({
-            image,
-            desciption,
-            title,
-            link,
-          });
+app.get("/", (req, res) => {
+  axios(urlBase)
+    .then((getHtml) => {
+      const html = getHtml.data;
+      const $ = cheerio.load(html);
+      const dataFromGit = [];
+      $(".col-10", html).each(function () {
+        const image =
+          "https://img.icons8.com/clouds/500/000000/motivation-daily-quotes.png";
+        const title = $(this).find("a").text().replace(/\s+/g, " ").trim();
+        const desciption = $(this).find("p").text().replace(/\s+/g, " ").trim();
+        const link = $(this).find("a").attr("href");
+        dataFromGit.push({
+          image,
+          desciption,
+          title,
+          link,
         });
-        // console.log(dataFromGit);
-        res.json(dataFromGit);
-      })
-      .catch((err) => console.log(err));
-})
+      });
+      // console.log(dataFromGit);
+      res.json(dataFromGit);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
